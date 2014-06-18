c36f1cf52aee9fa65bf477597e80a01cbfdbe239
Detry322 <Jserrino@gmail.com>
2014-06-18 13:20:23 -0500

    Add Bathroom Tracking Functionality

--- app/controllers/bathroom_controller.rb
+++ app/controllers/bathroom_controller.rb
@@ -1,8 +1,34 @@
 class BathroomController < ApplicationController
+
+  if BathroomVisit.last.nil?
+    BathroomVisit.create_session.deactivate
+
   def index
+    @open = BathroomVisit.in_use?
   end
 
   def update
-    head :ok
+    if params[:secret] == BathroomMonitor::SECRET
+      if ['open','closed'].include?(params[:status])
+        last_visit = BathroomVisit.last
+        if params[:status] == 'closed'
+          if last_visit.active?
+            last_visit.update_end_time
+          else
+            last_visit.create_session
+          end
+        elsif params[:status] == 'open'
+          if last_visit.active?
+            last_visit.deactivate
+            last_visit.update_end_time
+          end
+        end
+        head :ok
+      else
+        head :bad_request
+      end
+    else
+      head :forbidden
+    end
   end
 end
--- app/models/bathroom_visit.rb
+++ app/models/bathroom_visit.rb
@@ -1,2 +1,26 @@
 class BathroomVisit < ActiveRecord::Base
+
+  def self.create_session
+    BathroomVisit.create(start_time: Time.now, end_time: Time.now, active: true);
+  end
+
+  def self.in_use?
+    BathroomVisit.last.active?
+  end
+
+  def activate
+    self.active = true
+    self.save
+  end
+
+  def deactivate
+    self.active = false
+    self.save
+  end
+
+  def update_end_time
+    self.end_time = Time.now
+    self.save
+  end
+
 end
