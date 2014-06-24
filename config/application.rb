d56e4baea1b3825621d86d2e0979a669a4d364d9
Detry322 <Jserrino@gmail.com>
2014-06-23 21:21:23 -0500

    Remove secret key

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module BathroomMonitor
 
-  SECRET = 'seluyrjgtiw34i736gyfukalerbgoeauybehlrfbk'
+  SECRET = '<secret goes here>'
 
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
